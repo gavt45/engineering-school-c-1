16f98fd8b3fad78ebb903757096603600f8031f5
Gumirov Andrey <andreyt45@gmail.com>
2020-09-09 20:25:59 +0700

    Updated task #10 in contest_2. Now passing tests

--- contest_2/10.c
+++ contest_2/10.c
@@ -10,15 +10,19 @@ int main(){
     //start: n: 100 num of digits for now: 189 t: 188
     //start: n: 1000 num of digits for now: 2889 t: 2887
     int power10 = 1;
-    while (i+power10*pow(10,power10)-(power10-1)*pow(10,power10-1) < k)
+    while (i+9*power10*(int)pow(10,power10-1) < k){
+        i+=9*power10*(int)pow(10,power10-1);
+        n=pow(10,power10);
+        t=i-2;
         power10 += 1;
+    }
     while (i <= k){
-        printf("start: n: %d num of digits for now: %d t: %d\n", n, i-1, t);
+        // printf("start: n: %d num of digits for now: %d t: %d\n", n, i-1, t);
         t=i;
         if (i+(int)log10(n)+1 >= k){
-            printf("Before while: n: %d i: %d t: %d\n", n, i, t);
+            // printf("Before while: n: %d i: %d t: %d\n", n, i, t);
             while (i<=t+(int)log10(n)){
-                printf("n: %d i: %d t: %d; DIGIT: %d\n", n, i, t, (n/(int)pow(10,((int)log10(n) - (i-t))))%10);
+                // printf("n: %d i: %d t: %d; DIGIT: %d\n", n, i, t, (n/(int)pow(10,((int)log10(n) - (i-t))))%10);
                 if (i == k) {
                     printf("%d", (n/(int)pow(10,((int)log10(n) - (i-t))))%10);
                     return 0;
@@ -26,9 +30,9 @@ int main(){
                 i++;
             }
         } else {
-            printf("n: %d i: %d t: %d\n", n, i, t);//, (n/(int)pow(10,((int)log10(n)+1 - (i-t))))%10);
+            // printf("n: %d i: %d t: %d\n", n, i, t);//, (n/(int)pow(10,((int)log10(n)+1 - (i-t))))%10);
             i+=(int)log10(n)+1;
-            printf("n: %d num of digits for now: %d t: %d\n", n, i-1, t);
+            // printf("n: %d num of digits for now: %d t: %d\n", n, i-1, t);
         }
         n++;
     }
