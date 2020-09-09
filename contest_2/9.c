14c85c666e02585383030f6e0794fbfcb3e8da26
Gumirov Andrey <andreyt45@gmail.com>
2020-09-09 21:01:33 +0700

    Updated task #9. Not using built-in functions.

--- contest_2/9.c
+++ contest_2/9.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
-#define F(x,n) pow(-1, (n))*pow((x),2*(n)+1)/(2*(n)+1)
+// #define F(x,n) pow(-1, (n))*pow((x),2*(n)+1)/(2*(n)+1)
+#define ABS(x) ((x)>=0.0?(x):(-(x)))
 
 int main(){
     int k;
-    double x = 0.0,eps=0.99999,y=0.0;
+    double x = 0.0,eps=0.99999,y=0.0,mn;
     // int imin=-1;
     scanf("%lf %lf", &x, &eps);
     int i = 0;
-    y=F(x,0);
-    while (fabs(F(x,i)) >= eps)
-    {
+    y=x;
+    mn = x;
+    while (ABS(mn/(2*i+1)) >= eps) {
         i++;
-        y+=F(x,i);
+        mn *= -1*x*x;
+        y+=mn/(2*i+1);   
     }
     printf("%.5f",y);
     return 0;
